f0562018dd97c6d5cba594bd4fc2ccce1a344173
loldebyte <qdimarellis@laposte.net>
2021-04-07 18:08:21 +0200

    add drag& drop

--- src/components/board-user.component.js
+++ src/components/board-user.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import UserService from "../services/user.service";
+import Dropzone from "../Dropzone";
 
 export default class BoardUser extends Component {
   constructor(props) {
@@ -37,6 +38,7 @@ export default class BoardUser extends Component {
         <header className="jumbotron">
           <h3>{this.state.content}</h3>
         </header>
+        <Dropzone onDrop={this.onDrop} accept={"text/csv"}/>
       </div>
     );
   }
